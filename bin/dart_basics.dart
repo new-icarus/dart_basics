3edb7c7a45cc28200a74139281aba9456959f9bb
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-24 18:08:45 -0300

    functions parameters

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,12 +1,38 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  //Functions == Methods
-  //if nested function -> can only be called inside it
   print(returnsString());
 }
 
-//top level function
 String returnsString() {
   return 'hello';
+}
+
+void positionalParams(int x, double y, String greeting) {
+  //parameters (up) | arguments (down)
+  positionalParams(
+      5, 3.5, 'hi'); //must respect the order and provide all arguments
+}
+
+void optionalPositionalParams(int x, double y, [String? greeting = 'hi']) {
+  optionalPositionalParams(5, 3.5);
+  optionalPositionalParams(5, 3.5, 'holla');
+}
+
+void namedOptionalParams({
+  int? width,
+  double? height,
+  String? greeting,
+}) {
+  namedOptionalParams(greeting: 'hi', width: 5, height: 2.2);
+  namedOptionalParams(greeting: 'holla');
+}
+
+void namedRequiredParams(
+  int positional, {
+  required int? width,
+  required double? height,
+  required String? greeting,
+}) {
+  namedRequiredParams(10, greeting: 'hi', width: 5, height: 2.2);
 }

69bdfec418d6d205c2c643688cdc9daf74b7c732
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-24 16:21:51 -0300

    operators

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,6 +1,26 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  String? possible = 'abc';
-  print(possible.length);
+  int result;
+
+  result = 5 + 5;
+  result -= 5;
+  result *= 5;
+  // result /= 5; cannot happen because it's not a double
+  result ~/= 5; //integer result of a division
+  double floatResult = result % 5; //remainder of a division
+
+  int x = 0;
+  x++; //1
+  x--; //0
+  ++x; //1(add first)
+  --x; //0(subtract first)
+
+  bool isEqual = (5 == 10); //true
+  bool isGreaterOrEqual = (5 >= 10); //false
+  bool isLesser = (5 < 10); //true
+
+  String concatenation = 'String' + " Interpolation";
+
+  print('$concatenation. This is a cool feature. ${result + x} ');
 }

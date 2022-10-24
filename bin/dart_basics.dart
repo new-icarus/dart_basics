4be7c6ace31003cb767d12ccd63fadff187f88d8
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-24 16:13:01 -0300

    nullability

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,10 +1,6 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  var myString;
-  myString = 'Update';
-  print(myString);
-
-  myString = 5.5;
-  print(myString);
+  String? possible = 'abc';
+  print(possible.length);
 }

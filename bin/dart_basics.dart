927c4737e1c751d8c90c28cded3397f5064154a6
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-24 17:58:52 -0300

    functions/methods

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,21 +1,12 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  for (int i = 0; i < 10; i++) {
-    print('Run nº $i');
-  }
+  //Functions == Methods
+  //if nested function -> can only be called inside it
+  print(returnsString());
+}
 
-  bool condition = true;
-  while (condition) {
-    print('while loop');
-    if (condition) {
-      condition = false;
-    }
-  }
-
-  bool anotherCondition = false;
-
-  do {
-    print('I got printed!');
-  } while (anotherCondition);
+//top level function
+String returnsString() {
+  return 'hello';
 }

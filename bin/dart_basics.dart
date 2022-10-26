6df70d386cb9e094330bd5893b2ab0e781797d6e
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-25 21:25:43 -0300

    higher order functions. finally I understood this crap/awesome thing

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,38 +1,15 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  print(returnsString());
+  final twicePlusFive = twice((x) => x + 5);
+
+  print(twicePlusFive(3));
 }
 
-String returnsString() {
-  return 'hello';
-}
+typedef IntTransformer = int Function(int);
 
-void positionalParams(int x, double y, String greeting) {
-  //parameters (up) | arguments (down)
-  positionalParams(
-      5, 3.5, 'hi'); //must respect the order and provide all arguments
-}
-
-void optionalPositionalParams(int x, double y, [String? greeting = 'hi']) {
-  optionalPositionalParams(5, 3.5);
-  optionalPositionalParams(5, 3.5, 'holla');
-}
-
-void namedOptionalParams({
-  int? width,
-  double? height,
-  String? greeting,
-}) {
-  namedOptionalParams(greeting: 'hi', width: 5, height: 2.2);
-  namedOptionalParams(greeting: 'holla');
-}
-
-void namedRequiredParams(
-  int positional, {
-  required int? width,
-  required double? height,
-  required String? greeting,
-}) {
-  namedRequiredParams(10, greeting: 'hi', width: 5, height: 2.2);
+IntTransformer twice(IntTransformer f) {
+  return (int x) {
+    return f(f(x));
+  };
 }

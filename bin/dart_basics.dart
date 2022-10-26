74c1945395b57890e7c05bc987c90d258a08cec1
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-25 22:25:39 -0300

    collections. lists | maps | sets

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,15 +1,30 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  final twicePlusFive = twice((x) => x + 5);
+  List myList = [1, 2, 3];
+  print(myList.length);
 
-  print(twicePlusFive(3));
-}
+  final firstElement = myList[0]; //dynamic type because list default
+  print(firstElement);
 
-typedef IntTransformer = int Function(int);
+  List<int> myIntList = [1, 2, 3];
+  final intElement = myIntList[0]; //int type
+  print(intElement);
 
-IntTransformer twice(IntTransformer f) {
-  return (int x) {
-    return f(f(x));
+  final myList3 = [1, 2, 3]; //int list by type inference
+  final myList4 = [1, 2, 3, 'hello']; //object type by type inference
+
+  <int>[1, 2, 3]; //list literal
+
+  Map<String, dynamic> myMap = {
+    //JSON style
+    'name': 'John Doe',
+    'age': 42,
+    'registered': true,
   };
+  final name = myMap['name'];
+  print(name);
+
+  Set<int> mySet = {1, 2, 3, 2}; //non-duplicated values
+  print(mySet.length);
 }

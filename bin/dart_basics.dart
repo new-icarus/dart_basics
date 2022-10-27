2cfe0f19cfc13552492544499576b7d11e7d28f2
Tiago Macedo <mcdcsr.tiago@gmail.com>
2022-10-26 21:13:24 -0300

    classes and objects

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -3,20 +3,44 @@ import 'package:dart_basics/dart_basics.dart' as dart_basics;
 enum AccountType { free, premium, vip }
 
 void main(List<String> arguments) {
-  final userAccountType = AccountType.premium; //infered AccountType count
-  print(userAccountType.index);
+//instantiated new string
+  String myString = 'something';
 
-  print(AccountType.values[2]);
+//constructor invoked to create a new object -> Building|Cookie
+  User myUser = const User(
+    name: 'John Doe',
+    photoUrl: 'http://example.com/abcd',
+  );
 
-  switch (userAccountType) {
-    case AccountType.free:
-      print('0 USD');
-      break;
-    case AccountType.premium:
-      print('20 USD');
-      break;
-    case AccountType.vip:
-      print('40 USD');
-      break;
+  User myUser2 = const User(
+    name: 'John Doe',
+    photoUrl: 'http://example.com/abcd',
+  );
+
+  User myUser3 = User(
+    name: 'John Doe',
+    photoUrl: 'http://example.com/abcd',
+  );
+//performance improvement by using const
+//only one object instantiated
+
+  print(myUser == myUser2);
+  print(myUser == myUser3);
+
+  var testMethod = myUser3.hasLongName();
+  print(testMethod);
+}
+
+class User {
+//Blueprint|Cookie Cutter
+  final String name;
+  final String photoUrl;
+
+//Creating a field with non-nullable positional parameters
+  const User({required this.name, required this.photoUrl});
+
+//method
+  bool hasLongName() {
+    return name.length > 10;
   }
 }
